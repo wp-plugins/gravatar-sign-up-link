14c2425fa25a6776e843440ed8f69bec1b0ea0af
Tyrun <Tyrun@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-14 23:17:24 +0000

    added support for custom text
    git-svn-id: https://plugins.svn.wordpress.org/gravatar-sign-up-link/trunk@505109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravatar-signup-up-link.php
+++ gravatar-signup-up-link.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gravatar Sign Up Link
 Description: Automatically add a sign up link to Gravatar on your WordPress comment area.
-Version: 0.1
+Version: 1.0
 Author: Aaron Holbrook
 Author URI: http://aaronjholbrook.com/
 Plugin URI: http://a7web.com/plugins/
@@ -26,10 +26,75 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-function gsul_echo_link() {
-	echo '<a href="http://en.gravatar.com/">Get a Gravatar</a>';
+
+/*------------------------------------*\
+	Add Admin Option Page
+\*------------------------------------*/
+add_action('admin_menu', 'gsul_admin_add_page');
+function gsul_admin_add_page() {
+	add_options_page('Gravatar Sign Up Page', 'Gravatar Sign Up', 'manage_options', 'plugin', 'gsul_options_page');
 }
 
-add_action ( 'comment_form', 'gsul_echo_link' );
+/*------------------------------------*\
+	Display the admin options page
+\*------------------------------------*/
+function gsul_options_page() { ?>
+<div>
+	<h2>Gravatar Sign Up Link</h2>
+	Change the link text
+	<form action="options.php" method="post">
+		<?php settings_fields('gsul_options'); ?>
+		<?php do_settings_sections('gsul'); ?>	
 
-?>
+		<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+	</form>
+</div> <?php 	
+}
+
+/*------------------------------------*\
+	Add the admin settings and such
+\*------------------------------------*/
+add_action('admin_init', 'gsul_admin_init');
+function gsul_admin_init() {
+	register_setting( 'gsul_options', 'gsul_options', 'gsul_options_validate' );
+	add_settings_section( 'gsul_main', 'Main Settings', 'gsul_section_text', 'gsul');
+	add_settings_field( 'gsul_text_string', 'Gravatar Sign Up Link Text', 'gsul_setting_string', 'gsul', 'gsul_main' );
+}
+
+/*------------------------------------*\
+	Display the options
+\*------------------------------------*/
+function gsul_section_text() {
+	echo '<p>Main description of this section here.</p>';
+}
+
+function gsul_setting_string() {
+	$options = get_option('gsul_options');
+	echo "<input id='gsul_text_string' name='gsul_options[text_string]' size='40' type='text' value='{$options['text_string']}' />";
+}
+
+/*------------------------------------*\
+	Validate our options
+\*------------------------------------*/
+function gsul_options_validate($input) {
+	$newinput['text_string'] = trim($input['text_string']);
+	return $newinput;
+}
+
+
+
+
+
+/*------------------------------------*\
+	Add the link
+\*------------------------------------*/
+add_action ( 'comment_form', 'gsul_echo_link' );
+function gsul_echo_link() {
+	$custom_text = get_option('gsul_options');
+	$custom_text = $custom_text['text_string'];
+	if( $custom_text == '') {
+		$custom_text = 'Get a Gravatar';
+	}
+	echo '<a href="http://en.gravatar.com/">'.$custom_text.'</a>';
+}
+
